d05b7ca287d77f056fda1c1f064069429a8de975
aldi <aldiirsanmajid@gmail.com>
2023-08-07 09:49:50 +0700

    auth_provider.g.dart ketinggalan

---
+++ lib/logic/state_management/riverpod/auth_provider/auth_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authStatusHash() => r'fb4e7871606f93487ad8858bbbae25e8723990b1';
+
+/// See also [AuthStatus].
+@ProviderFor(AuthStatus)
+final authStatusProvider =
+    AutoDisposeAsyncNotifierProvider<AuthStatus, AuthResponse?>.internal(
+  AuthStatus.new,
+  name: r'authStatusProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$authStatusHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthStatus = AutoDisposeAsyncNotifier<AuthResponse?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
